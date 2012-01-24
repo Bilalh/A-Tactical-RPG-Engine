21fa4d5b4e2b0f119be9856d6af63af556f48c38
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2012-01-24 20:50:51 +0000

    printf style logging for log4j

---
+++ src/config/LogF.java
@@ -0,0 +1,47 @@
+package config;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ * printf style logging for log4j
+ * @author Bilal Hussain
+ */
+public class LogF {
+
+    private static final String FQCN = LogF.class.getName();
+
+    
+	public static void trace(Logger log, String fmt, Object... args) {
+		if (log.isEnabledFor(Level.TRACE)) {
+	        log.callAppenders(new LoggingEvent(FQCN, log, Level.TRACE, String.format(fmt, args), null));
+		}
+	}
+
+	public static void debug(Logger log, String fmt, Object... args) {
+		if (log.isEnabledFor(Level.DEBUG)) {
+	        log.callAppenders(new LoggingEvent(FQCN, log, Level.DEBUG, String.format(fmt, args), null));
+		}
+	}
+
+	public static void info(Logger log, String fmt, Object... args) {
+		if (log.isEnabledFor(Level.INFO)) {
+	        log.callAppenders(new LoggingEvent(FQCN, log, Level.INFO, String.format(fmt, args), null));
+		}
+	}
+
+	public static void warn(Logger log, String fmt, Object... args) {
+		if (log.isEnabledFor(Level.WARN)) {
+	        log.callAppenders(new LoggingEvent(FQCN, log, Level.WARN, String.format(fmt, args), null));
+		}
+	}
+
+	public static void error(Logger log, String fmt, Object... args) {
+		if (log.isEnabledFor(Level.ERROR)) {
+	        log.callAppenders(new LoggingEvent(FQCN, log, Level.ERROR, String.format(fmt, args), null));
+		}
+	}
+
+}
