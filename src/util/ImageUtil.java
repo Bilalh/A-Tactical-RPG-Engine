6839cad88359186503ed8b9e3d475301525146ad
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2012-02-08 18:39:51 +0000

    image methods

---
+++ src/util/ImageUtil.java
@@ -0,0 +1,49 @@
+package util;
+
+import java.awt.*;
+import java.awt.image.*;
+
+/**
+ * @author Bilal Hussain
+ */
+public class ImageUtil {
+
+	public static Image setColorToTransparent(BufferedImage img, final Color c) {
+		ImageFilter filter = new ColorToTransparent(c);
+		ImageProducer ip = new FilteredImageSource(img.getSource(), filter);
+		return Toolkit.getDefaultToolkit().createImage(ip);
+	}
+	
+	public static BufferedImage createBufferedImage(Image sourceImage) {
+		final int width = sourceImage.getWidth(null), height = sourceImage.getHeight(null);
+		GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
+		BufferedImage bimg = gc.createCompatibleImage(width, height, Transparency.BITMASK);
+		Graphics g = bimg.getGraphics();
+		g.drawImage(sourceImage, 0, 0, width, height, null);
+		g.dispose();
+		return bimg;
+
+	}
+
+	static class ColorToTransparent extends RGBImageFilter{
+		
+		private Color colour;
+		private int rgbMask;
+		
+		/** @category Generated */
+		public ColorToTransparent(Color colour) {
+			this.colour = colour;
+			rgbMask = colour.getRGB() | 0xFF000000;
+		}
+		
+		@Override
+		public final int filterRGB(int x, int y, int rgb) {
+			if ((rgb | 0xFF000000) == rgbMask) {
+				// Mark the alpha bits as zero meaning transparent
+				return 0x00FFFFFF & rgb;
+			}
+			return rgb;
+		}
+	}
+	
+}
