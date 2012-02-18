33726ed1e127abb6013086fddb3848e92a2c19a2
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2012-02-18 23:02:19 +0000

    fix

--- src/view/ui/Menu.java
+++ src/view/ui/Menu.java
@@ -8,70 +8,71 @@ import java.util.List;
 
 /**
  * A menu has a number of items that can be chosen either using the keyboard or the mouse.
+ * 
  * @author Bilal Hussain
  */
 public class Menu implements IMenu {
 
 	private int xOffset = 25;
 	private int yOffset = 20;
-	
+
 	private int selected = 0;
-	
-	private RoundRectangle2D.Float area  = new RoundRectangle2D.Float();
+
+	private RoundRectangle2D.Float area = new RoundRectangle2D.Float();
 	private List<MenuItem> commands;
-	
+
 	@Override
-	public void draw(Graphics2D g, int drawX, int drawY){
+	public void draw(Graphics2D g, int drawX, int drawY) {
 		g = (Graphics2D) g.create();
 
-		area =  new RoundRectangle2D.Float(drawX, drawY,
+		area = new RoundRectangle2D.Float(drawX, drawY,
 				70,
-				25*commands.size(),
+				25 * commands.size(),
 				10, 10);
-		
+
 		Composite oldC = g.getComposite();
-		
-		AlphaComposite alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER,0.5f);
+
+		AlphaComposite alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f);
 		Color oldCo = g.getColor();
 		g.setComposite(alphaComposite);
 		g.setColor(Color.green);
 		g.fill(area);
-		
+
 		g.setComposite(oldC);
 		g.setColor(oldCo);
 
-		g.translate(drawX+xOffset, drawY+yOffset);
+		g.translate(drawX + xOffset, drawY + yOffset);
 
-        g.setRenderingHint ( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		for (MenuItem m : commands) {
 			m.draw(g, m == commands.get(selected));
 			g.translate(0, 20);
 		}
-		
+
 	}
 
 	@Override
-	public void scrollToPrevious(){
-		selected = (selected-1 + commands.size()) % commands.size();
+	public void scrollToPrevious() {
+		selected = (selected - 1 + commands.size()) % commands.size();
 	}
-	
+
 	@Override
-	public void scrollToNext(){
-		selected = (selected+1) % commands.size();
+	public void scrollToNext() {
+		selected = (selected + 1) % commands.size();
 	}
-	
+
 	@Override
-	public MenuItem getClickedItem(Point p){
-		if (area.contains(p)){
-			float index = p.y-area.y;
-			selected = (int) (index/25) ;
+	public MenuItem getClickedItem(Point p) {
+		if (area.contains(p)) {
+			float index = p.y - area.y;
+			selected = (int) (index / 25);
 			return commands.get(selected);
 		}
 		return null;
 	}
-	
+
 	@Override
-	public void addCommand(MenuItem m){
+	public void addCommand(MenuItem m) {
 		commands.add(m);
 	}
 
@@ -80,17 +81,16 @@ public class Menu implements IMenu {
 		this.commands = commands;
 		reset();
 	}
-	
+
 	@Override
-	public void reset(){
-		selected=0;
+	public void reset() {
+		selected = 0;
 	}
-	
+
 	@Override
-	public void clear(){
+	public void clear() {
 		commands.clear();
 	}
-	
 
 	@Override
 	public MenuItem getSelected() {
