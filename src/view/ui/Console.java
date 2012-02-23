20ef3bfd6ead55b0d42b729ace08ecda3270540f
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2012-02-23 23:34:18 +0000

    Console

--- src/view/ui/Console.java
+++ src/view/ui/Console.java
@@ -3,13 +3,14 @@ package view.ui;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
-
-import org.apache.log4j.Logger;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 import view.ui.interfaces.IConsole;
 
 /**
- * A Console displays uneditable text graphically to the user. Draw has to be called to draw the textarea
+ * A Console displays uneditable text graphically to the user. paint has to be called to draw the textarea
  * The textArea has a finite history which can scroll backwards and fowards. The number of lines shown is 
  * specifed at creation and the resulting height of the textarea can be found using getHeight.
  * 
@@ -17,54 +18,59 @@ import view.ui.interfaces.IConsole;
  */
 public class Console implements IConsole {
 
-	private static final Logger log = Logger.getLogger(Console.class);
-
-	// lines is a circular list
+	/** lines is a circular list */
 	private String[] lines;
-	// index is the next free postion,
+	/** index is the next free postion, */
 	private int index;
-	// head is the index in lines to print from
+	/** head is the index in lines to print from */
 	private int head;
-	// Number of lines to show
+	/** Number of lines to show */
 	private final int numberOfLinesToShow;
-	// Height in pixels
+	/** Height in pixels */
 	private final int height;
-	// Width in pixels
+	/** Width in pixels */
 	private int width;
 
-	// To make sure only the last n lines are shown
+	/** To make sure only the last n lines are shown */
 	private boolean around;
-	// lines are numbered if true
-	private boolean numberLines;
+	/** Lines are numbered if true */
+	private final boolean numberedLines;
+	/** The current line number */
 	private int lineIndex;
 
-	private boolean printToSysout;
+	/** If not null the text is printed the stream */
+	private PrintStream out;
 
+	private Font font = new Font("MONOSPACED", Font.PLAIN, 12);
+	
 	public Console() {
-		this(1000, 7, true);
+		this(1000, 7, true,null);
 	}
 
-	public Console(int history, int linesToShow, boolean numberLines) {
+	public Console(int history, int linesToShow, boolean numberedLines, PrintStream stream) {
 		if (linesToShow <= 0 || linesToShow > history || history <= 0) throw new IllegalArgumentException("Invaild Arguments");
 		
-		lines = new String[history];
-		index = head = lineIndex = 0;
+		lines  = new String[history];
+		index  = head = lineIndex = 0;
 		around = false;
-		printToSysout = true;
+		out    = stream;
 		height = (int) ((17f) * linesToShow);
+
+		this.numberedLines  = numberedLines;
 		numberOfLinesToShow = linesToShow;
 	}
 
-	// Adds a string to the Text area, the string will be printed on a new line
-	// Note: Escape sequences such as \n are not honoured.
+	/**
+	 * Adds a string to the Text area, the string will be printed on a new line
+	 * Note: Escape sequences such as \n are not honoured.
+	 */
 	@Override
 	public void println(Object newObj) {
 		if (newObj == null) throw new IllegalArgumentException("String can not be null");
 
 		String newString = newObj.toString();
-		if (printToSysout) log.info(newObj);
-
-		lines[index] = numberLines ? newString : (lineIndex++) + " " + newString;
+		if (out != null) out.println(newString);
+		lines[index] = numberedLines ? newString : (lineIndex++) + " " + newString;
 
 		index++;
 		if (index == lines.length) around = true;
@@ -79,26 +85,27 @@ public class Console implements IConsole {
 		}
 	}
 
-	// Adds a string to the Text area, the string will be printed on a new line
-	// Note: Escape sequences such as \n are not honoured.
+	/**
+	 * Adds a string to the Text area, the string will be printed on a new line
+	 * Note: Escape sequences such as \n are not honoured.
+	 */
 	@Override
 	public void printf(String format, Object... args) {
 		println(String.format(format, args));
 	}
 
-	// Draw the text area at the specifed point, with the specifed width.
+	/** Draw the text area at the specifed point, with the specifed width. */
 	@Override
 	public void draw(Graphics2D g, int drawX, int drawY) {
-		Color originalColour = g.getColor();
-		Font originalFont = g.getFont();
+		Color old = g.getColor();
+		Font oldFont = g.getFont();
 
 		// Draw the background
 		g.setColor(new Color(0, 0, 0, 150));
 		g.fillRect(drawX, drawY, width, height);
 
 		g.setColor(Color.white);
-		g.setFont(new Font("MONOSPACED", Font.PLAIN, 12));
-		// graphics2D.drawString(ii + "", 100, 100);
+		g.setFont(font);
 
 		// draw each line
 		for (int i = 0, j = head; i < numberOfLinesToShow; i++, j = (j + 1) % lines.length) {
@@ -106,57 +113,79 @@ public class Console implements IConsole {
 			g.drawString(lines[j], drawX + 5, drawY + 12 + (17 * i));
 		}
 
-		g.setColor(originalColour);
-		g.setFont(originalFont);
+		g.setColor(old);
+		g.setFont(oldFont);
 	}
 
-	// Scroll backwards by one line
-	// Note: since the list is circular the list will wrap around if the start is reached.
-	// Note: the text area is scrolled to the end on reciving new input.
+	/**
+	 * Scroll backwards by one line
+	 * Note: since the list is circular the list will wrap around if the start is reached.
+	 * Note: the text area is scrolled to the end on reciving new input.
+	 */
 	@Override
 	public void scrollUp() {
 		head = (head - 1 + lines.length) % lines.length;
 	}
 
-	// Scroll forwards by one line
-	// Note: since the list is circular the list will wrap around if the end is reached.
-	// Note: the text area is scrolled to the end on reciving new input.
+	/**
+	 * Scroll forwards by one line
+	 * Note: since the list is circular the list will wrap around if the start is reached.
+	 * Note: the text area is scrolled to the end on reciving new input.
+	 */
 	@Override
 	public void scrollDown() {
 		head = (head + 1) % lines.length;
 	}
 
-	// Scroll backwards by one page
-	// Note: since the list is circular the list will wrap around if the end is reached.
-	// Note: the text area is scrolled to the end on reciving new input.
+	/**
+	 * Scroll backwards by one page
+	 * Note: since the list is circular the list will wrap around if the start is reached.
+	 * Note: the text area is scrolled to the end on reciving new input. 
+	 */
 	@Override
 	public void pageUp() {
 		head = (head - numberOfLinesToShow + lines.length) % lines.length;
 	}
 
-	// Note: since the list is circular the list will wrap around if the end is reached.
-	// Note: the text area is scrolled to the end on reciving new input.
+	/**
+	 * Note: since the list is circular the list will wrap around if the start is reached.
+	 * Note: the text area is scrolled to the end on reciving new input.
+	 */
 	@Override
 	public void pageDown() {
 		head = (head + numberOfLinesToShow) % lines.length;
 	}
 
-	// Return the height in xel of the text area
+	/**
+	 * Returns the height in pixels of the text area.
+	 */
 	@Override
 	public int getHeight() {
 		return height;
 	}
 
-	/** @category Generated */
+	/**
+	 * Returns the width in pixels of the text area.
+	 */
 	@Override
 	public int getWidth() {
 		return width;
 	}
 
-	/** @category Generated */
+	/**
+	 * Sets the width of the console
+	 */
 	@Override
 	public void setWidth(int width) {
 		this.width = width;
+	}
+
+	/**
+	 * Set the extra stream to print to
+	 */
+	@Override
+	public void setPrintStream(PrintStream out) {
+		this.out = out;
 	}
 
 }
--- src/view/ui/interfaces/IConsole.java
+++ src/view/ui/interfaces/IConsole.java
@@ -1,39 +1,77 @@
 package view.ui.interfaces;
 
 import java.awt.Graphics2D;
+import java.io.PrintStream;
 
 /**
+ * A Console displays uneditable text graphically to the user. paint has to be called to draw the textarea
+ * The Console has a finite history which can scroll backwards and fowards.
  * @author Bilal Hussain
  */
 public interface IConsole extends IDisplayable {
 
-	// Adds a string to the Text area, the string will be printed on a new line
+
+	/**
+	 * Adds a string to the Text area, the string will be printed on a new line
+	 * Note: Escape sequences such as \n are not honoured.
+	 */
 	void println(Object newObj);
 
-	// Adds a string to the Text area, the string will be printed on a new line
+	/**
+	 * Adds a string to the Text area, the string will be printed on a new line
+	 * Note: Escape sequences such as \n are not honoured.
+	 */
 	void printf(String format, Object... args);
 
-	// Draw the text area at the specifed point, with the specifed width.
+	/** Draw the text area at the specifed point, with the specifed width. */
 	@Override
 	void draw(Graphics2D g, int drawX, int drawY);
-	
-	// Scroll backwards by one line
+
+	/**
+	 * Scroll backwards by one line
+	 * Note: since the list is circular the list will wrap around if the start is reached.
+	 * Note: the text area is scrolled to the end on reciving new input.
+	 */
 	void scrollUp();
 
-	// Scroll forwards by one line
+	/**
+	 * Scroll forwards by one line
+	 * Note: since the list is circular the list will wrap around if the start is reached.
+	 * Note: the text area is scrolled to the end on reciving new input.
+	 */
 	void scrollDown();
 
-	// Scroll backwards by one page
+	/**
+	 * Scroll backwards by one page
+	 * Note: since the list is circular the list will wrap around if the start is reached.
+	 * Note: the text area is scrolled to the end on reciving new input. 
+	 */
 	void pageUp();
 
-	// Scroll forwards by one page
+	/**
+	 * Note: since the list is circular the list will wrap around if the start is reached.
+	 * Note: the text area is scrolled to the end on reciving new input.
+	 */
 	void pageDown();
 
-	// Return the height in xel of the text area
+	/**
+	 * Returns the height in pixels of the text area.
+	 */
 	int getHeight();
 
+	/**
+	 * Returns the width in pixels of the text area.
+	 */
 	int getWidth();
 
+	/**
+	 * Sets the width of the console
+	 */
 	void setWidth(int width);
+
+	/**
+	 * Set the extra stream to print to
+	 */
+	void setPrintStream(PrintStream out);
 	
 }
