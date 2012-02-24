57e39c06f5c1a3806aa73d2b9bca5aeeb0ab2a3b
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2012-02-24 17:47:42 +0000

    storing skiils and weapons

---
+++ src/engine/AssertStore.java
@@ -0,0 +1,43 @@
+package engine;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import common.gui.ResourceManager;
+import common.gui.Sprite;
+import common.interfaces.IWeapon;
+import engine.skills.ISkill;
+
+/**
+ * Keeps Track of all the item, skills.
+ * @author Bilal Hussain
+ */
+public class AssertStore {
+
+	private static AssertStore singleton = new AssertStore();
+	private Map<UUID, IWeapon> weapons = Collections.synchronizedMap(new HashMap<UUID, IWeapon>());
+	private Map<UUID, ISkill> skills   = Collections.synchronizedMap(new HashMap<UUID, ISkill>());
+
+	public  IWeapon getWeapon(UUID id){
+		final IWeapon w =  weapons.get(id);
+		assert w != null : "Assert with id not found: " + id ;
+		return w;
+	}
+	
+	public  ISkill getSkills(UUID id){
+		final ISkill s =  skills.get(id);
+		assert s != null : "Assert with id not found: " + id ;
+		return s;
+	}
+	
+	//FIXME load weapons and skills
+	
+	private AssertStore() {
+	}
+	
+	public static AssertStore instance() {
+		return singleton;
+	}
+}
