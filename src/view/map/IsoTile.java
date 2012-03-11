2febebb345b8ccd464e432fd92ca2b29e245fb12
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2012-03-11 17:45:50 +0000

    Drawing

--- src/view/map/IsoTile.java
+++ src/view/map/IsoTile.java
@@ -58,7 +58,10 @@ public class IsoTile {
 	protected float height;
 	protected float startHeight;
 	protected float endHeight;
-
+	
+	protected boolean outline    = true;
+	protected Color outlineColor = Color.BLACK;
+	
 	protected Polygon  top; // For mouse testing 
 	protected Location fieldLocation;
 
@@ -189,7 +192,7 @@ public class IsoTile {
 		switch (orientation) {
 			case TO_NORTH:
 			case TO_SOUTH:
-				drawNorthSouth(x, y, g, false);
+				drawNorthSouth(x, y, g,false, false);
 				break;
 			case TO_EAST:
 			case TO_WEST:
@@ -202,28 +205,18 @@ public class IsoTile {
 		}
 	}
 
-	Color lineColor = Color.BLACK;
 	public void drawEastWest(int x, int y, Graphics _g, boolean topPloy, boolean topOnly) {
 		Graphics2D g = (Graphics2D) _g;
 		Color oldColor = g.getColor();
 		Paint old = g.getPaint();
 
-//		final int x_hor_div_2         = (int) (x + horizontal / 2);
-//		final int neg_x_hor_div_2     = (int) (x - horizontal / 2);
-//		final int y_vet               = (int) (y + vertical);
-//		final int y_vet_div_2         = (int) (y + vertical / 2);
-//		final int neg_y_h1_vet_div_2  = (int) (y - h1 + vertical / 2);
-//		final int neg_y_h2_vet_div_2  = (int) (y - h2 + vertical / 2);
-//		final int neg_y_h2_vet        = (int) (y - h2 + vertical);
-		final int x_hor_div_2         =Math.round(x + horizontal / 2);
-		final int neg_x_hor_div_2     =Math.round(x - horizontal / 2);
-		final int y_vet               =Math.round(y + vertical);
-		final int y_vet_div_2         =Math.round(y + vertical / 2);
-		final int neg_y_h1_vet_div_2  =Math.round(y - h1 + vertical / 2);
-		final int neg_y_h2_vet_div_2  =Math.round(y - h2 + vertical / 2);
-		final int neg_y_h2_vet        =Math.round(y - h2 + vertical);
-		
-		
+		final int x_hor_div_2         = (x + horizontal / 2);
+		final int neg_x_hor_div_2     = (x - horizontal / 2);
+		final int y_vet               = (y + vertical);
+		final int y_vet_div_2         = (y + vertical / 2);
+		final int neg_y_h1_vet_div_2  = (y - h1 + vertical / 2);
+		final int neg_y_h2_vet_div_2  = (y - h2 + vertical / 2);
+		final int neg_y_h2_vet        = (y - h2 + vertical);
 		
 		top = new Polygon(new int[] {
 				x,
@@ -237,42 +230,7 @@ public class IsoTile {
 						neg_y_h1_vet_div_2 }, 4);
 		
 		if (topPloy && !topOnly) return;
-		
-		if (type == TEXTURED) {
-			g.setPaint(ResourceManager.instance().getTexturedTile(tileName));
-			g.fillPolygon(top);
-			g.setPaint(old);
-		}else if(startHeight != endHeight){
-			log.trace("Using safety textured tile, since heights differ");
-			g.setPaint(tGrass);
-			g.fillPolygon(top);
-			g.setPaint(old);			
-		} else {
-			int f = Math.round(x - horizontal / 2f);
-			g.drawImage(tileImage,  f, (y - h2), null);
-		}
-
-
-		// Draw highlighting
-		if (state != TileState.NONE) {
-			Composite oldC = g.getComposite();
-			AlphaComposite alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f);
-			g.setComposite(alphaComposite);
-			g.setColor(state.colour);
-			g.fillPolygon(top);
-			g.setComposite(oldC);
-		}
-		
-		// highlighting tile if selected
-		if (isSelected() || topOnly) {
-			Composite oldC = g.getComposite();
-			AlphaComposite alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f);
-			g.setComposite(alphaComposite);
-			g.setColor(TileState.SELECTED.colour);
-			g.fillPolygon(top);
-			g.setComposite(oldC);
-		}
-		
+		drawTop(x, y, topOnly, g, old);
 		// return if we are only the top polygon (for editor)
 		if (topPloy) return;
 
@@ -294,19 +252,6 @@ public class IsoTile {
 		g.fillPolygon(poly);
 		g.setPaint(old);
 
-//			g.setColor(lineColor); // Outline
-//			g.drawPolygon(new Polygon(new int[] {
-//					x,
-//					x_hor_div_2,
-//					x_hor_div_2,
-//					x },
-//					new int[] {
-//							neg_y_h2_vet,
-//							neg_y_h2_vet_div_2,
-//							y_vet_div_2,
-//							y_vet }
-//					, 4));
-
 		// Draw the left wall
 		poly = new Polygon(new int[] {
 				x,
@@ -324,51 +269,92 @@ public class IsoTile {
 		g.fillPolygon(poly);
 		g.setPaint(old);
 
-//			g.setColor(lineColor); // Outline
-//			g.drawPolygon(new Polygon(new int[] {
-//					x,
-//					neg_x_hor_div_2,
-//					neg_x_hor_div_2,
-//					x },
-//					new int[] {
-//							neg_y_h2_vet,
-//							neg_y_h1_vet_div_2,
-//							y_vet_div_2,
-//							y_vet }
-//					, 4));
+		if (outline){
+			g.setColor(outlineColor); // Outline right wall
+			g.drawPolygon(new Polygon(new int[] {
+					x,
+					x_hor_div_2,
+					x_hor_div_2,
+					x },
+					new int[] {
+							neg_y_h2_vet,
+							neg_y_h2_vet_div_2,
+							y_vet_div_2,
+							y_vet }
+					, 4));
+			
+				g.setColor(outlineColor); // Outline left wall
+				g.drawPolygon(new Polygon(new int[] {
+						x,
+						neg_x_hor_div_2,
+						neg_x_hor_div_2,
+						x },
+						new int[] {
+								neg_y_h2_vet,
+								neg_y_h1_vet_div_2,
+								y_vet_div_2,
+								y_vet }
+						, 4));
 
-//
-//		g.setColor(lineColor); // Outline the top of the tile
-//		g.drawPolygon(new Polygon(new int[] {
-//				x,
-//				x_hor_div_2,
-//				x,
-//				neg_x_hor_div_2 },
-//				new int[] {
-//						(int) (y - h1),
-//						neg_y_h2_vet_div_2,
-//						neg_y_h2_vet,
-//						neg_y_h1_vet_div_2 }
-//				, 4));
-//		
+			g.setColor(outlineColor); // Outline the top of the tile
+			g.drawPolygon(new Polygon(new int[] {
+					x,
+					x_hor_div_2,
+					x,
+					neg_x_hor_div_2 },
+					new int[] {
+							(y - h1),
+							neg_y_h2_vet_div_2,
+							neg_y_h2_vet,
+							neg_y_h1_vet_div_2 }
+					, 4));
+		}
 		g.setColor(oldColor);
 	}
 
-	/**
-	 * Draw a tile that slants to the north or the south. The methods for drawing are very similar,
-	 * so they can be merged into one method.
-	 * 
-	 * @category unused
-	 */
-	public void drawNorthSouth(int x, int y, Graphics g, boolean toponly) {
+	private void drawTop(int x, int y, boolean topOnly, Graphics2D g, Paint old) {
+		
+		if (type == TEXTURED) {
+			g.setPaint(ResourceManager.instance().getTexturedTile(tileName));
+			g.fillPolygon(top);
+			g.setPaint(old);
+		}else if(startHeight != endHeight){
+			log.trace("Using safety textured tile, since heights differ");
+			g.setPaint(tGrass);
+			g.fillPolygon(top);
+			g.setPaint(old);			
+		} else {
+			int f = Math.round(x - horizontal / 2f);
+			g.drawImage(tileImage,  f, (y - h2), null);
+		}
 
-		int h1 = orientation == Orientation.TO_NORTH ? (int) (finalHeight * startHeight)
-				: (int) (finalHeight * endHeight);
-		int h2 = orientation == TO_NORTH ? (int) (finalHeight * endHeight)
-				: (int) (finalHeight * startHeight);
+		// Draw highlighting
+		if (state != TileState.NONE) {
+			Composite oldC = g.getComposite();
+			AlphaComposite alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f);
+			g.setComposite(alphaComposite);
+			g.setColor(state.colour);
+			g.fillPolygon(top);
+			g.setComposite(oldC);
+		}
+		
+		// highlighting tile if selected
+		if (isSelected() || topOnly) {
+			Composite oldC = g.getComposite();
+			AlphaComposite alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f);
+			g.setComposite(alphaComposite);
+			g.setColor(TileState.SELECTED.colour);
+			g.fillPolygon(top);
+			g.setComposite(oldC);
+		}
+	}
 
+	
+	public void drawNorthSouth(int x, int y, Graphics _g, boolean topPloy, boolean topOnly) {
+		Graphics2D g = (Graphics2D) _g;
 		Color oldColor = g.getColor();
-		g.setColor(state.colour);
+		Paint old = g.getPaint();
+
 		top = new Polygon(new int[] {
 				x,
 				x + horizontal / 2,
@@ -379,11 +365,14 @@ public class IsoTile {
 						y - h1 + vertical,
 						y - h1 + vertical / 2 }
 				, 4);
-		if (toponly) return;
 		
-		g.fillPolygon(top);
 		
-		g.fillPolygon(new Polygon(new int[] {
+		if (topPloy && !topOnly) return;
+		drawTop(x, y, topOnly, g, old);
+		// return if we are only the top polygon (for editor)
+		if (topPloy) return;
+		
+		Polygon poly = new Polygon(new int[] {
 				x,
 				x + horizontal / 2,
 				x + horizontal / 2,
@@ -393,9 +382,14 @@ public class IsoTile {
 						y - h2 + vertical / 2,
 						y + vertical / 2,
 						y + vertical }
-				, 4));
+				, 4);
 
-		g.fillPolygon(new Polygon(new int[] {
+		old = g.getPaint();
+		g.setPaint(rightWall);
+		g.fillPolygon(poly);
+		g.setPaint(old);
+		
+		poly = new Polygon(new int[] {
 				x,
 				x - horizontal / 2,
 				x - horizontal / 2,
@@ -405,42 +399,54 @@ public class IsoTile {
 						y - h1 + vertical / 2,
 						y + vertical / 2,
 						y + vertical }
-				, 4));
-		g.setColor(Color.BLACK); // Outline the top of the tile
-		g.drawPolygon(new Polygon(new int[] {
-				x,
-				x + horizontal / 2,
-				x,
-				x - horizontal / 2 },
-				new int[] {
-						y - h2,
-						y - h2 + vertical / 2,
-						y - h1 + vertical,
-						y - h1 + vertical / 2 }
-				, 4));
-		// Outline the right side
-		g.drawPolygon(new Polygon(new int[] {
-				x,
-				x + horizontal / 2,
-				x + horizontal / 2,
-				x },
-				new int[] {
-						y - h1 + vertical,
-						y - h2 + vertical / 2,
-						y + vertical / 2,
-						y + vertical }
-				, 4));
-		// Outline the left side
-		g.drawPolygon(new Polygon(new int[] {
-				x,
-				x - horizontal / 2,
-				x - horizontal / 2,
-				x },
-				new int[] { y - h1 + vertical,
-						y - h1 + vertical / 2,
-						y + vertical / 2,
-						y + vertical }
-				, 4));
+				, 4);
+		
+		old = g.getPaint();
+		g.setPaint(leftWall);
+		g.fillPolygon(poly);
+		g.setPaint(old);
+	
+		if (outline){
+			g.setColor(outlineColor);
+			// Outline right.
+			g.drawPolygon(new Polygon(new int[] {
+					x,
+					x + horizontal / 2,
+					x + horizontal / 2,
+					x },
+					new int[] {
+							y - h1 + vertical,
+							y - h2 + vertical / 2,
+							y + vertical / 2,
+							y + vertical }
+					, 4));
+			
+			// Outline left.
+			g.drawPolygon(new Polygon(new int[] {
+					x,
+					x - horizontal / 2,
+					x - horizontal / 2,
+					x },
+					new int[] { y - h1 + vertical,
+							y - h1 + vertical / 2,
+							y + vertical / 2,
+							y + vertical }
+					, 4));
+			
+			// Outline top
+			g.drawPolygon(new Polygon(new int[] {
+					x,
+					x + horizontal / 2,
+					x,
+					x - horizontal / 2 },
+					new int[] {
+							y - h2,
+							y - h2 + vertical / 2,
+							y - h1 + vertical,
+							y - h1 + vertical / 2 }
+					, 4));
+			
+		}
 		g.setColor(oldColor);
 	}
 
