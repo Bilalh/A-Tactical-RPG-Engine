298676de0c771357aae49153dfe6872fdf754243
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2012-03-11 01:41:41 +0000

    scripts

--- scripts/create_iso_tiles.sh
+++ scripts/create_iso_tiles.sh
@@ -2,6 +2,7 @@
 # Creates isometric tiles from square images (either jpeg or png). 
 # Place the sources image in  the `images' folder.
 # The results will be placed in the `results` folder
+# requires imagemagick  (http://www.imagemagick.org/) 
 pushd images
 for i in *; do
 	convert -trim -rotate 45 -trim -resize x50% -transparent white "$i" "../results/${i%.*}.png"
