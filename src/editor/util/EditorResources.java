06f6fee7ef2b9170bd3967766e9b9bcf1f7adc1a
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2012-03-13 00:13:12 +0000

    image path case bugfix

--- src/editor/util/EditorResources.java
+++ src/editor/util/EditorResources.java
@@ -9,6 +9,8 @@ import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
+import org.apache.log4j.Logger;
+
 import editor.Editor;
 import editor.map.MapEditor;
 
@@ -17,18 +19,22 @@ import editor.map.MapEditor;
  * @author Bilal Hussain
  */
 public final class EditorResources {
-
+	private static final Logger log = Logger.getLogger(EditorResources.class);
+	
 	private EditorResources() {
 	}
 
 	
 	public static InputStream getFileAsStream(String filename) {
-		return Editor.class.getResourceAsStream("resources/" + filename);
+		InputStream s =  Editor.class.getResourceAsStream("/editor/resources/" + filename);
+		log.info(filename);
+		if (s == null) throw new IllegalArgumentException(filename + "not found");
+		return s;
 	}
 	
 	public static BufferedImage getImage(String filename) {
 		try {
-			return ImageIO.read(Editor.class.getResourceAsStream("resources/" + filename));
+			return ImageIO.read(getFileAsStream(filename));
 		} catch (IOException e) {
 			System.err.println("Failed to load  image: " + filename);
 			e.printStackTrace();
